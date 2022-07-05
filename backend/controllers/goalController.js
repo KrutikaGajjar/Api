ac0efdbd22371f3f3d49ca0d63a33aec17fd8239
KrutikaGajjar <krutikagajjar20@gmail.com>
2022-07-05 11:23:52 +0530

    adding

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,27 +1,31 @@
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 const asyncHandler = require('express-async-handler');
 const Bank = require('../models/goalModel')
 
 const getGoals = asyncHandler(async (req,res) => {
     const banks = await Bank.find()
     res.status(200).json(banks)
+    
 })
 
 const setGoal = asyncHandler(async (req,res) => {
-    if(!req.body.name, !req.body.email, !req.body.status){
+    // const {email,pwd} = req.body 
+    if(!req.body.pwd, !req.body.email){
         res.status(400)
-        throw new Error('please add a text')
+        throw new Error('please add a post text')
     }
     
-const date = new Date;
-let hours = date.getHours();
-let status = (hours < 12)? "Morning" :
-             ((hours <= 18 && hours >= 12 ) ? "Afternoon" : "Night");
-res.send(status);
+// const date = new Date;
+// let hours = date.getHours();
+// let status = (hours < 12)? "Morning" :
+//              ((hours <= 18 && hours >= 12 ) ? "Afternoon" : "Night");
+// res.send(status);
     
     const bank = await Bank.create({
-        name: req.body.name,
+        pwd: req.body.pwd,
         email: req.body.email,
-        status: req.body.status,
+        // status: req.body.status,
     })
     res.status(200).json(bank)
 })
